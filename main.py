9c233d9af23720c9eba1e2f18ece89e8ebbabab9
NP <76001641+CoderN-P@users.noreply.github.com>
2021-11-11 02:30:12 +0000

    added a bunch of stuff like Ajax and login pages to make the website really good

--- main.py
+++ main.py
@@ -1,11 +1,56 @@
 import flask
+from flask import render_template, request, jsonify, make_response, url_for
+from mongo_methods import check_device, get_device, check_signup, update_password
+
 
 app = flask.Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html")
+    return render_template("index.html")
+
+@app.route('/setlogin', methods=['POST'])
+def connect():
+    data1 = request.get_json()
+    ip = data1['ip']
+    password = data1['password']
+
+
+    correct = check_device(ip, password)
+  
+    return correct
+
+@app.route('/dashboard')
+def dashboard():
+  if 'ip' in request.cookies and 'password' in request.cookies:
+    return render_template('dashboard.html')
+  else:
+    return render_template('login.html')
+    
+
+
+@app.route('/signupcheck', methods=['POST'])
+def signup1():
+  data = request.get_json() 
+  ip = data['ip']
+  password = data['password']
+
+  correct = check_signup(ip)
+  if correct == 'true':
+    update_password(ip, password)
+  return correct
+
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')
+  
+
+@app.route('/login')
+def login_page():
+  return render_template("login.html")
+
+
 
 
 if __name__ == "__main__":
---
+++ mongo_methods.py
@@ -0,0 +1,32 @@
+import pymongo, dns
+import os
+
+
+client = pymongo.MongoClient(os.getenv('MONGO'))
+registered_devices = client.main_data.registered_devices
+
+def check_device(ip, password):
+  if not registered_devices.count_documents({"_id": ip}) or not registered_devices.count_documents({'Password': password}):
+    return 'false'
+  return 'true'
+
+def get_device(ip, password):
+  data = registered_devices.find_one({"_id": ip, "password": password})
+
+  return dict(data)
+
+def check_signup(ip):
+  print(ip)
+  if registered_devices.count_documents({"_id": ip}):
+    data = registered_devices.find_one({"_id":ip})
+    if data['Password'] == None:
+      return 'true'
+    else:
+      return 'false'
+  else:
+    return 'false'
+
+def update_password(ip, password):
+  registered_devices.update_one({"_id":ip}, {"$set": {'Password': password}})
+
+
